b543ef96f152839d5606827e4af7c09e20927fe1
Jason Denning <jason@ngeniux.com>
2014-01-14 20:07:16 -0800

    Cleaned up module documentation

--- dynamo_pillar.py
+++ dynamo_pillar.py
@@ -6,6 +6,24 @@ Use an Amazon Web Services DynamoDB table as a Pillar data source
 Installation:
 -------------
 
+    - Create a directory on the salt master for custom extension modules (if you don't already have one)
+      e.g. /srv/salt/ext
+
+    - In your master config (e.g. /etc/salt/master):
+
+.. code-block:: yaml
+
+    extension_modules : /srv/salt/ext
+
+    - Create a subdirectory of the extension modules directory named 'pillar'
+
+    - Copy this dynamo_pillar.py to the pillar subdirectory
+
+    - Make sure that you have the python boto library installed (`pip install boto`)
+
+    - Create a boto configuration file to hold your AWS credentials
+      (see: http://code.google.com/p/boto/wiki/BotoConfig)
+
 
 
 Configuration:
@@ -19,7 +37,42 @@ In the master config file:
         - dynamo:
             table: my_pillar_table  # REQUIRED - set to your DynamoDB table name
             region: us-east-1       # Optional - default is 'us-east-1'
-            id_field: id            # Optional - the field to query for minion_id; default is 'id'
+            id_field: id            # Optional - the field to query for minion's id; default is 'id'
+
+
+
+Usage:
+------
+
+ - Create a DynamoDB table with a primary hash key field named `id`
+
+ - Create a record for each minion, with the minion's id in the primary hash key field
+
+ - Create additional key-value pairs for each value you want to store in the pillar
+
+ - Keys which include the '.' character, will be transformed into a hierarchical python dict
+        e.g. if your DynamoDB record looks like this:
+
+            ssh.port = 22
+            ssh.password.authentication = yes
+            ssh.password.permitEmpty = no
+            foo.bar.baz = fiz
+
+        the returned pillar for the node will be:
+
+            {'ssh' : {
+                'port' : '22',
+                'password' : {
+                    'authentication' : 'yes',
+                    'permitEmpty' : no,
+                },
+             'foo' : {
+                 'bar' : {
+                     'baz' : 'fiz'
+                 }}}
+
+
+
 '''
 
 __author__ = 'Jason Denning <jason@ngeniux.com>'
